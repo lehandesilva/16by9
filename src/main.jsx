317f5d0a30b97bb8f98fd11bcb26a806409f72ac
NumeralArcher <lehande28@gmail.com>
2023-08-04 01:54:56 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App";
 
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
